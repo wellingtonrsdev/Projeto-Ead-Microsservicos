91f334648ae2f8dacc29854d7a3f873678cae49a
wellington-workstation <rodrigueswellington3@gmail.com>
2026-03-11 17:07:27 -0300

    [chores]: crud completo do microsserviço course

--- authuser/src/main/java/com/ead/authuser/AuthuserApplication.java
+++ authuser/src/main/java/com/ead/authuser/AuthuserApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthuserApplication {
 
 	public static void main(String[] args) {
+		System.out.println(">>> CONFIG LOADED <<<");
 		SpringApplication.run(AuthuserApplication.class, args);
 	}
 
